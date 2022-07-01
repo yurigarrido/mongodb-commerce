40df7386dfb78127b3f0892001e8df1531b2dda1
Yuri Garrido <y.garrido.yuri@gmail.com>
2022-06-30 23:53:09 -0300

    req 19 - done

---
+++ challenges/desafio19.js
@@ -0,0 +1,17 @@
+// Remova o item cebola de todos os sanduíches
+// Para isso, escreva no arquivo desafio19.js duas queries, nesta ordem:
+
+// Crie uma query que faça a remoção do item cebola em todos os sanduíches.
+
+db.produtos.updateMany({}, {
+  $pull: {
+    ingredientes: "cebola",
+  },
+});
+
+// Crie uma query que retorne o nome e ingredientes de todos os documentos.
+db.produtos.find({}, {
+  nome: true,
+  ingredientes: true,
+  _id: false,
+});

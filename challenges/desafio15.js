25c65b2a17cefabe5572b147253ab8b1b6066ad2
Yuri Garrido <y.garrido.yuri@gmail.com>
2022-06-30 23:11:11 -0300

    req 15 - done

---
+++ challenges/desafio15.js
@@ -0,0 +1,33 @@
+// Crie uma query que inclua o campo avaliacao do tipo NumberInt, com o valor 0 em todos os documentos da coleção.
+db.produtos.updateMany(
+  {},
+  {
+    $set: {
+      avaliacao: NumberInt(0),
+    },
+  },
+);
+// Crie uma query que incremente o valor do campo avaliacao em 5 em todos os sanduíches de carne do tipo bovino.De olho na dica: utilize como filtro o campo tags.
+db.produtos.updateMany(
+  { tags: { $all: ["bovino"] } },
+  {
+    $inc: {
+      avaliacao: 5,
+    },
+  },
+);
+
+// Crie uma query que incremente o valor do campo avaliacao em 3 em todos os sanduíches de ave.
+db.produtos.updateMany(
+  { tags: { $in: ["ave"] } },
+  {
+    $inc: {
+      avaliacao: 3,
+    },
+  },
+);
+// Crie uma query que retorne o nome e avaliacao de todos os sanduíches.
+db.produtos.find(
+  {},
+  { _id: false, nome: true, avaliacao: true },
+);
